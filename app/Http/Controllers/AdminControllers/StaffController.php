b37ec576a0e1d04ab02fea6bc2863571318e56ba
Suvo-32 <hsuvo020@gmail.com>
2025-01-09 18:58:18 +0530

    stafflist api for admin

--- app/Http/Controllers/AdminControllers/StaffController.php
+++ app/Http/Controllers/AdminControllers/StaffController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AdminControllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StaffController extends Controller
@@ -13,6 +14,28 @@ class StaffController extends Controller
     public function index()
     {
         //
+        $stafflist = User::with('userDetails')->get()->toArray();
+// dd($stafflist);
+        $data  = collect($stafflist)->map(function($item){
+          return [
+               "id" => $item['id'],
+               "staff_id" => $item['staff_id'],
+               "ippis_no" => $item['ippis_no'],
+               "name" => $item['F_name']." ".$item['M_name']." ".$item['L_name'],
+               "email" => $item['email'],
+               "is_active" => $item['user_details']['is_active'],
+               "type" => "user",
+               "grade_level" => $item['user_details']['grade_level'],
+          ];
+        });
+
+        if($data->isEmpty()){
+            return response()->json(['message' => 'No staff found'], 404);
+        }   
+        else {
+            return response()->json(['data' => $data], 200);
+        }
+        // dd($data); 
     }
 
     /**
